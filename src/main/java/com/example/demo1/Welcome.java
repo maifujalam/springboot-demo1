13a867f76acef2ea859f3324c1e7f26cea49d14b
alam <sk.maifujalam@gmail.com>
2024-02-03 22:58:12 +0530

    first commit

---
+++ src/main/java/com/example/demo1/Welcome.java
@@ -0,0 +1,12 @@
+package com.example.demo1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome Demo-1";
+    }
+}
